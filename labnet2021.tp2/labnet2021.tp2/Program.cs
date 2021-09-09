113d9a845d8812c6f71819ff8a74ba2ba4431ab2
BrunoG <bruno.g97@gmail.com>
2021-09-09 11:50:57 -0300

    Segunda entrega ejercicio 1 POO
    Se implementaron correcciones sobre el código original

--- labnet2021.tp2/labnet2021.tp2/Program.cs
+++ labnet2021.tp2/labnet2021.tp2/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace labnet2021.tp2
 {
@@ -13,112 +9,107 @@ namespace labnet2021.tp2
 
             do
             {
-                Console.Clear();
-                Console.WriteLine("LabNet 2021 - TP 2 \n");
-                Console.WriteLine("Por favor proceda con la carga de la cantidad de pasajeros de los siguientes omnibus:");
-
                 TransportePublico[] transportes = new TransportePublico[10];
 
-
-                for (int i = 0;i < 5; i++)
-                {
-                    Console.WriteLine($"Indique la cantidad de pasajeros del ómnibus N° {i+1} (entre 0 y 30) \n");
-
-                    bool valorCorrecto = false;
-
-                    do
-                    {
-                        if (int.TryParse(Console.ReadLine(), out int number))
-                        {
-                            if (number > 30 || number < 0)
-                            {
-                                Console.WriteLine("Cantidad incorrecta, intente nuevamente");
-                                continue;
-                            }
-
-                            Omnibus o = new Omnibus();
-                            o.Pasajeros = number;
-                            transportes[i] = o;
-                            valorCorrecto = true;
-                        }
-
-                        else
-                        {
-                            Console.WriteLine("El valor ingresado no es un número apropiado, intente nuevamente");
-                            continue;
-                        }
-
-                    }
-                    while (valorCorrecto == false);
-                        
-                }
-                Console.Clear();
-
-                Console.WriteLine("LabNet 2021 - TP 2 \n");
-                Console.WriteLine("Carga de pasajeros de ómnibus concluída, por favor continúe con la carga de pasajeros de los taxis");
-
-                for (int i = 5; i < 10; i++)
-                {
-                    Console.WriteLine($"Indique la cantidad de pasajeros del taxi N° {i -4} (entre 0 y 4) \n");
-
-                    bool valorCorrecto = false;
-
-                    do
-                    {
-                        if (int.TryParse(Console.ReadLine(), out int number))
-                        {
-                            if (number > 4 || number < 0)
-                            {
-                                Console.WriteLine("Cantidad incorrecta, intente nuevamente");
-                                continue;
-                            }
-
-                            Omnibus o = new Omnibus();
-                            o.Pasajeros = number;
-                            transportes[i] = o;
-                            valorCorrecto = true;
-                        }
-
-                        else
-                        {
-                            Console.WriteLine("El valor ingresado no es un número apropiado, intente nuevamente");
-                            continue;
-                        }
-
-                    }
-                    while (valorCorrecto == false);
-
-                }
-
-
-
-
-                Console.Clear();
-                Console.WriteLine("LabNet 2021 - TP 2 \n");
-                Console.WriteLine("Carga finalizada, los valores añadidos son los siguientes: \n");
-                for(int i = 0;i<5;i++)
-                {
-                    Console.WriteLine($"Cantidad de pasajeros del ómnibus N° {i+1}: { transportes[i].Pasajeros}");
-
-                }
-                Console.WriteLine();
-                for (int i = 5; i < 10; i++)
-                {
-                    Console.WriteLine($"Cantidad de pasajeros del taxi N° {i-4}: { transportes[i].Pasajeros}");
-                }
-                Console.WriteLine();
-                Console.WriteLine("Presione una tecla para reiniciar la carga");
-                Console.ReadKey();
-
+                CargaDeOmnibus(transportes);
+                CargaDeTaxis(transportes);
+                MuestraDeCarga(transportes);
             }
 
             while (true);
 
+            void MostrarEncabezado()
+            {
+                Console.Clear();
+                Console.WriteLine("LabNet 2021 - TP 2 \n");
+            }
 
-            Console.ReadLine();
+            void CargaDeOmnibus(TransportePublico[] arregloTransporte)
+            {
+                MostrarEncabezado();
+
+                Console.WriteLine("Por favor proceda con la carga de la cantidad de pasajeros de los siguientes omnibus:");
+
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine($"Indique la cantidad de pasajeros del ómnibus N° {i + 1} (entre 0 y 30) \n");
+
+                    int cantidadPasajerosInput = ValidacionReadline(30);
+
+                            Omnibus o = new Omnibus();
+                            o.Pasajeros = cantidadPasajerosInput;
+                            arregloTransporte[i] = o;
+               }
+                           
+            }
+          
+            void CargaDeTaxis(TransportePublico[] arregloTransporte)
+            {
+                MostrarEncabezado();
+
+                Console.WriteLine("Carga de pasajeros de ómnibus concluída, por favor continúe con la carga de pasajeros de los taxis");
+
+                for (int i = 5; i < 10; i++)
+                {
+                    Console.WriteLine($"Indique la cantidad de pasajeros del taxi N° {i - 4} (entre 0 y 4) \n");
+
+                    int cantidadPasajerosInput = ValidacionReadline(4);
+
+                    Omnibus o = new Omnibus();
+                    o.Pasajeros = cantidadPasajerosInput;
+                    arregloTransporte[i] = o;
+                }
+
+            }
+          
+            void MuestraDeCarga(TransportePublico[] arregloTransporte)
+            {
+                MostrarEncabezado();
+
+                Console.WriteLine("Carga finalizada, los valores añadidos son los siguientes: \n");
+
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine($"Cantidad de pasajeros del ómnibus N° {i + 1}: { arregloTransporte[i].Pasajeros}");
+
+                }
+                Console.WriteLine();
+                for (int i = 5; i < 10; i++)
+                {
+                    Console.WriteLine($"Cantidad de pasajeros del taxi N° {i - 4}: { arregloTransporte[i].Pasajeros}");
+                }
+                Console.WriteLine();
+                Console.WriteLine("Presione una tecla para reiniciar la carga");
+                Console.ReadKey();
+            }
+        
+            int ValidacionReadline(int cantidadMaxima)
+            {
+                do
+                {
+                    if (int.TryParse(Console.ReadLine(), out int number))
+                    {
+
+                        if (number > cantidadMaxima || number < 0)
+                        {
+                            Console.WriteLine("Cantidad incorrecta, intente nuevamente");
+                            continue;
+                        }
+
+                        else
+                        {
+                            return number;
+                        }
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("El valor ingresado no es un número apropiado, intente nuevamente");
+                        continue;
+                    }
+                }
+                while (true);
+            }
         }
-    
-       
-    
     }
 }
